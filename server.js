004c4ba10503c1b17102746720f358bb281542d8
Tahir <dibirovtahir@gmail.com>
2023-01-31 02:24:54 +0400

    CORS

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import connectToDB from './config/connectToDb.js';
 import router from './routes/postRouter.js';
+import cors from 'cors'
+
 
 if(process.env.NODE_ENV !="production"){
     dotenv.config();
 }
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
